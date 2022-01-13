38c5def50522927bfeb6431528a19b478613b09f
Rolwane <rolwane@hotmail.com>
2022-01-13 14:19:29 -0300

    Requisito 02

---
+++ src/lib/constants.js
@@ -0,0 +1,2 @@
+export const MIN_LENGTH = 3; // número mínimo de caracteres do input de login
+export const temp = null;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,69 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../../services/userAPI';
+import { MIN_LENGTH } from '../../lib/constants';
+
+// imported components
+import Loading from '../../components/Loading';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: '',
+      isButtonDisabled: true,
+      isLoadingVisible: false,
+      isLogged: false,
+    };
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      input: value,
+      isButtonDisabled: value.length < MIN_LENGTH,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { input: name } = this.state;
+
+    this.setState({ isLoadingVisible: true });
+
+    createUser({ name }).then(() => {
+      this.setState({ isLoadingVisible: false, isLogged: true });
+    });
+  }
+
   render() {
-    return <div data-testid="page-login">Login</div>;
+    const {
+      input, isButtonDisabled, isLoadingVisible, isLogged } = this.state;
+
+    return (
+      <div data-testid="page-login">
+        <form onSubmit={ this.handleSubmit }>
+
+          <input
+            type="text"
+            data-testid="login-name-input"
+            value={ input }
+            onChange={ this.handleChange }
+          />
+
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ isButtonDisabled }
+          >
+            Entrar
+          </button>
+
+        </form>
+        {isLoadingVisible && <Loading />}
+        {isLogged && <Redirect to="/search" />}
+      </div>
+    );
   }
 }
 
