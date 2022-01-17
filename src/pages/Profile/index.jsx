ce853868a8229d177a1e1ac8a9980c625bc3b2db
Rolwane <rolwane@hotmail.com>
2022-01-17 15:10:19 -0300

    Requisito 13

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../../services/userAPI';
 
 // imported components
 import Header from '../../components/Header';
+import Loading from '../../components/Loading';
 
 class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userData: {},
+      isLoadingVisible: true,
+    };
+  }
+
+  componentDidMount() {
+    getUser().then((userData) => {
+      this.setState({ userData, isLoadingVisible: false });
+    });
+  }
+
   render() {
+    const { userData, isLoadingVisible } = this.state;
+    const { name, email, description, image } = userData;
+
     return (
       <div data-testid="page-profile">
         <Header active="profile" />
-        Profile
+        {
+          isLoadingVisible ? <Loading /> : (
+            <section className="profile-infos">
+              <img src={ image } alt="profile" data-testid="profile-image" />
+              <Link to="/profile/edit">Editar perfil</Link>
+              <p>{name}</p>
+              <p>{email}</p>
+              <p>{description}</p>
+            </section>
+          )
+        }
       </div>
     );
   }
